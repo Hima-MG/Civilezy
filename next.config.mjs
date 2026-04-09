5e0694abe05b3532da19e1f809369c08d1d9fcac
Thahsil07 <himaammu656@gmail.com>
2026-04-09 17:24:27 +0530

    Revert "Replace Next redirects with static _redirects"
    This reverts commit d8c24c05e680d1569605b0a7719f321439dfca93.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  async redirects() {
+    return [
+      {
+        source: "/courses",
+        destination: "https://learn.civilezy.in",
+        permanent: true,
+      },
+      {
+        source: "/course/:slug*",
+        destination: "https://learn.civilezy.in/course/:slug*",
+        permanent: true,
+      },
+      {
+        source: "/login",
+        destination: "https://learn.civilezy.in/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
